7ce09f1c9ef55bb32ddec87b7940cc48b34c1a9a
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-11-08 21:56:56 +0600

    Basic node mongoDB setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -7,9 +8,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p2sr91x.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 app.get('/', (req, res) => {
-    res.send('volunteer server site is running')
+    res.send('KidSpace server site is running')
 })
 app.listen(port, () => {
-    console.log(`volunteer server is running on port: ${port}`)
+    console.log(`KidSpace server is running on port: ${port}`)
 })
