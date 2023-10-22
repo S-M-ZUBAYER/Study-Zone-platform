eb8c6714ed338d22b989d6303a4ca88a67aaffbb
S-M-ZUBAYER <smzubayer18@gmail.com>
2023-10-22 15:48:29 +0600

    Last commit

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p2sr91x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//create a function for jwt token
 function verifyJwtToken(req, res, next) {
     const authenticHeader = req.headers.authorization;
     if (!authenticHeader) {
